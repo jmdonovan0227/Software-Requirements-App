e2826c43ca2e942202cc53a213505165e8ba585b
Jake D <jmdonovan0227@gmail.com>
2025-04-04 07:52:06 -0400

    Updated Navbar title

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function NavigationBar() {
     return (
         <div className='flex justify-between items-center w-full h-[10vh] border-b-2 border-gray-300 shadow-lg sticky top-0]'>
-            <h1 className='ml-2 font-sans text-xl md:text-2xl lg:text-3xl font-semibold'>SR App</h1>
+            <h1 className='ml-2 font-sans text-xl md:text-2xl lg:text-3xl font-semibold'>Software Requirements App</h1>
             <div className="flex items-center h-full mr-4">
                 <div className="mr-2">
                     <SignedOut>
